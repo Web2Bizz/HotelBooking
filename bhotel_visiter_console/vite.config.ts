2410534e2f9defce6c67edc41c2e984ea7a8d677
Alexey Volkov <alexvolkovdd@gmail.com>
2024-04-21 21:55:15 +0400

    fix(apps): фикс приложений

--- bhotel_visiter_console/vite.config.ts
+++ bhotel_visiter_console/vite.config.ts
@@ -4,20 +4,20 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 7770,
-  },
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@widgets': path.resolve(__dirname, './src/widgets'),
-      '@entities': path.resolve(__dirname, './src/entities'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@ui': path.resolve(__dirname, './src/shared/ui'),
-      '@api': path.resolve(__dirname, './src/shared/api')
-    },
-  },
-  plugins: [react()],
+	server: {
+		host: '0.0.0.0',
+		port: 5171
+	},
+	resolve: {
+		alias: {
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@ui': path.resolve(__dirname, './src/shared/ui'),
+			'@api': path.resolve(__dirname, './src/shared/api')
+		}
+	},
+	plugins: [react()]
 })
