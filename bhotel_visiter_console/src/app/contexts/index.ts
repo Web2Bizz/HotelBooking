956f4259ae40655f4a5c9debba45c197e7c29322
xarlein <alexvolkovdd@gmail.com>
2024-06-15 16:46:21 +0400

    fix

--- bhotel_visiter_console/src/app/contexts/index.ts
+++ bhotel_visiter_console/src/app/contexts/index.ts
@@ -1,4 +1,4 @@
 export { AddPriceTourContext } from './addTourContext'
-export { SearchContext } from './searchContext'
+// export { SearchContext } from './searchContext'
 export { UserContext, initialUserData } from './userContext'
 export type { TUserContext } from './userContext'
