151e28c2adbdfa74cd8d0b76831187744c6145d5
xarlein <alexvolkovdd@gmail.com>
2024-06-17 01:15:55 +0400

    filter

--- hotel-booking-server/service/room.service.js
+++ hotel-booking-server/service/room.service.js
@@ -1,89 +1,103 @@
-import client from "../db.js";
-import ApiError from "../exceptions/api-error.js";
-import { v4 as uuidv4 } from "uuid";
+import client from '../db.js'
+import ApiError from '../exceptions/api-error.js'
+import { v4 as uuidv4 } from 'uuid'
 
 class RoomService {
-  async getRoom() {
-    const response = await client.query(
-      `select 
+	async getRandomRoomAndRange() {
+		const response = await client.query(
+			`select 
       R.id_room, R.room_number, R.room_floor, R.facility, RT.room_type, RS.status, RS.color, R.price 
       from public.room as R 
       inner join public.roomtype as RT on R.id_room_type = RT.id_room_type
       inner join public.roomstatus as RS on R.id_status = RS.id_status 
       order by R.id_room asc `
-    );
-    return response;
-  }
+		)
+		return response
+	}
+	async getRandomRoom() {
+		const response = await client.query(
+			`select 
+      R.id_room, R.room_number, R.room_floor, R.facility, RT.room_type, RS.status, RS.color, R.price 
+      from public.room as R 
+      inner join public.roomtype as RT on R.id_room_type = RT.id_room_type
+      inner join public.roomstatus as RS on R.id_status = RS.id_status 
+      order by R.id_room asc LIMIT 5 OFFSET 2`
+		)
+		return response
+	}
 
-  async getCurrentRoom(id) {
-    const response = await client.query(
-      `select 
+	async getCurrentRoom(id) {
+		const response = await client.query(
+			`select 
       R.id_room, R.room_number, R.room_floor, R.facility, RT.room_type, RS.status, RS.color, R.price 
       from public.room as R 
       inner join public.roomtype as RT on R.id_room_type = RT.id_room_type
       inner join public.roomstatus as RS on R.id_status = RS.id_status
       WHERE id_room = '${id}'
       order by R.id_room asc `
-    );
-    return response;
-  }
-  async createRoom(
-    id_room_type,
-    room_floor,
-    id_status,
-    numberRoom,
-    targetKeys
-  ) {
-    let idRoom = uuidv4();
-    let status = "";
-    if (id_status === null || id_status === undefined) {
-      status = "024e26c7-5e33-4f35-a88e-e6f5c9322e02";
-    } else {
-      status = id_status;
-    }
+		)
+		return response
+	}
+	async createRoom(
+		id_room_type,
+		room_floor,
+		id_status,
+		numberRoom,
+		targetKeys
+	) {
+		let idRoom = uuidv4()
+		let status = ''
+		if (id_status === null || id_status === undefined) {
+			status = '024e26c7-5e33-4f35-a88e-e6f5c9322e02'
+		} else {
+			status = id_status
+		}
 
-    let hasAlreadyField = await client.query(
-      `select room_number from public.room where room_number = $1`,
-      [numberRoom]
-    );
-    if (
-      hasAlreadyField.rows[0] !== null &&
-      hasAlreadyField.rows[0] !== undefined
-    ) {
-      throw ApiError.BadRequest("Комната с данным номером уже существует");
-    }
+		let hasAlreadyField = await client.query(
+			`select room_number from public.room where room_number = $1`,
+			[numberRoom]
+		)
+		if (
+			hasAlreadyField.rows[0] !== null &&
+			hasAlreadyField.rows[0] !== undefined
+		) {
+			throw ApiError.BadRequest('Комната с данным номером уже существует')
+		}
 
-    await client.query(
-      `insert into public.room (id_room_type, room_floor, id_status, room_number, id_room, facility) 
+		await client.query(
+			`insert into public.room (id_room_type, room_floor, id_status, room_number, id_room, facility) 
         values ($1, $2, $3, $4, $5, $6)`,
-      [id_room_type, room_floor, status, numberRoom, idRoom, targetKeys]
-    );
-  }
-  async editRoom(
-    id_room,
-    id_room_type,
-    room_floor,
-    id_status,
-    numberRoom,
-    targetKeys
-  ) {
-    await client.query(
-      `update public.room set id_room_type = $1, room_floor = $2, room_number = $3, facility = $4 
+			[id_room_type, room_floor, status, numberRoom, idRoom, targetKeys]
+		)
+	}
+	async editRoom(
+		id_room,
+		id_room_type,
+		room_floor,
+		id_status,
+		numberRoom,
+		targetKeys
+	) {
+		await client.query(
+			`update public.room set id_room_type = $1, room_floor = $2, room_number = $3, facility = $4 
       where id_room = $5`,
-      [id_room_type, room_floor, numberRoom, targetKeys, id_room]
-    );
-  }
+			[id_room_type, room_floor, numberRoom, targetKeys, id_room]
+		)
+	}
 
-  async editRoomStatus(id_room) {
-    await client.query(`
+	async editRoomStatus(id_room) {
+		await client.query(
+			`
     update public.room set id_status = '024e26c7-5e33-4f35-a88e-e6f5c9322e02' where id_room = $1
-    `, [id_room])
-  }
+    `,
+			[id_room]
+		)
+	}
 
-  async deleteRoom(id_room) {
-    // await client.query(`delete from public.roomandfacility where id_room = $1`, [id_room]);
-    await client.query(`delete from public.room where id_room = $1`, [id_room]);
-  }
+	async deleteRoom(id_room) {
+		// await client.query(`delete from public.roomandfacility where id_room = $1`, [id_room]);
+		await client.query(`delete from public.room where id_room = $1`, [id_room])
+	}
 }
 
-export default RoomService;
+export default RoomService
--- hotel-booking-server/v2/index.js
+++ hotel-booking-server/v2/index.js
@@ -1,10 +1,14 @@
 import Router from 'express';
 import UserController from '../controller/user.controller.js';
+import RoomController from '../controller/room.controller.js';
 
 const userController = new UserController();
 const v2Router = new Router();
 
+const roomsController = new RoomController();
+
 v2Router.post('/registration', userController.RegistrateUser)
 v2Router.put('/user', userController.Editv2User)
+v2Router.post('/filter', roomsController.FilterRoom)
 
 export default v2Router;
